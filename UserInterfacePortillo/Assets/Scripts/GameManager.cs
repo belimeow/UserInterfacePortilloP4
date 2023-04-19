f4bd002023e056d51d95c0f056a686944f212a60
belimeow <711592@student.dusd.net>
2023-04-19 11:41:45 -0700

    life
    update Moonbin has passed away and I want to go home and also I did do thr keeping score thing and now yeahhh

--- UserInterfacePortillo/Assets/Scripts/GameManager.cs
+++ UserInterfacePortillo/Assets/Scripts/GameManager.cs
@@ -2,40 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
+
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
 
     private float spawnRate = 1.0f;
-
+    public bool isGameActive;
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
+
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
 
+        
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     IEnumerator SpawnTarget()
     {
-        while(true)
+        while(isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
 
-            UpdateScore(5);
+            
         }
     }
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
     // Update is called once per frame
     void Update()
--- UserInterfacePortillo/Assets/Scripts/Target.cs
+++ UserInterfacePortillo/Assets/Scripts/Target.cs
@@ -13,19 +13,42 @@ public class Target : MonoBehaviour
 
     private GameManager gameManager;
 
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        gameManager = GameObject.Find("Game Manager");
+        
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
 
-    }
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
+    }
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+            {
+                gameManager.GameOver();
+            }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -46,16 +69,7 @@ public class Target : MonoBehaviour
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
-
-    private void OnMouseDown()
-    {
-        Destroy(gameObject);
-
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        Destroy(gameObject);
-    }
+  
 
 
 }
